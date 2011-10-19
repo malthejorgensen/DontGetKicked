ffec6977878ce0a5eaa3bddad5f8042f8c93419a
Malthe Jørgensen <malthe.banan@gmail.com>
2011-10-20 00:48:13 +0200

    Add command-line arguments to train.py

--- train.py
+++ train.py
@@ -1,3 +1,21 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Train and test neural network on generated training and test data.')
+
+parser.add_argument('--network-file', default='brain.neuralnet', help='File where FANN neural network is stored.')
+
+parser.add_argument('--train-file', default='train.data', help='File where FANN training data is stored.')
+
+parser.add_argument('--test', action='store_true', help='Test neural network (instead of training it).')
+parser.add_argument('--test-file', default='test.data', help='File where FANN test data is stored.')
+
+#parser.add_argument('--skip-header', default=0, type=int, help='Skip this number of lines from beginning of file.')
+#parser.add_argument('--skip-footer', default=0, type=int, help='Skip this number of lines from end of file.')
+#parser.add_argument('--range', default='0,2000', help='The range of data rows to be output to the file ("START,END").')
+
+args = parser.parse_args()
+
+
 from pyfann import libfann
 
 def TestOnData(nn, testdata):
@@ -34,5 +52,7 @@ def TrainOnData(filename, output):
 
     ann.save(output)
 
-#TrainOnData("train.data","NeuralNetwork.out")
-TestOnData("NeuralNetwork.out", "test.data")
+if args.test:
+    TestOnData(args.network_file, args.test_file)
+else:
+    TrainOnData(args.train_file, args.network_file)
